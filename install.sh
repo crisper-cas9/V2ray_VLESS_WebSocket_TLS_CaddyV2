49f9e8c562fdf25ffae7e463cc2551d6b9134656
crisper-cas9 <107124776+crisper-cas9@users.noreply.github.com>
2022-07-30 11:54:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ apt install -y curl sudo jq qrencode
 echo
 echo -e "$yellow安装V2ray最新版本$none"
 echo "----------------------------------------------------------------"
-bash <(curl -L https://multi.netlify.app/v2ray.sh)
+bash <(curl -L https://raw.githubusercontent.com/Jrohy/multi-v2ray/master/v2ray.sh)
 
 systemctl enable v2ray
 
