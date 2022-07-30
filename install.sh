d94121bf8d27f3c30e5f8f0ef1fe1d392cea90cb
crisper-cas9 <107124776+crisper-cas9@users.noreply.github.com>
2022-07-30 12:12:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ if [ $# -ge 1 ]; then
     v2ray_id=$(cat /proc/sys/kernel/random/uuid)
     v2ray_port=$(shuf -i20001-65535 -n1)
     path=$(echo $v2ray_id | sed 's/.*\([a-z0-9]\{12\}\)$/\1/g')
-    proxy_site="https://zelikk.blogspot.com"
+    proxy_site="https://www.baidu.com/"
 
     echo -e "domain: ${domain}"
     echo -e "netstack: ${netstack}"
@@ -262,7 +262,7 @@ if [[ -z $proxy_site ]]; then
         echo -e "请输入 ${magenta}一个正确的 $none ${cyan}网址$none 用来作为 ${cyan}网站的伪装$none , 例如 https://zelikk.blogspot.com"
         echo "Input a camouflage site. When GFW visit your domain, the camouflage site will display."
         read -p "$(echo -e "(默认site: [${cyan}https://zelikk.blogspot.com$none]):")" proxy_site
-        [[ -z $proxy_site ]] && proxy_site="https://zelikk.blogspot.com"
+        [[ -z $proxy_site ]] && proxy_site="https://www.baidu.com/"
 
         case $proxy_site in
         *[#$]*)
