30c703e32c359cc5604a7d0c4e6dcd4664251fe7
crisper-cas9 <107124776+crisper-cas9@users.noreply.github.com>
2022-07-30 12:07:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ apt install -y curl sudo jq qrencode
 echo
 echo -e "$yellow安装V2ray最新版本$none"
 echo "----------------------------------------------------------------"
-bash <(curl -L https://raw.githubusercontent.com/Jrohy/multi-v2ray/master/v2ray.sh)
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
 systemctl enable v2ray
 
